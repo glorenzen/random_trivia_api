a2c6ab9f236c3d0b761d5fe4b6b9b08e922ee41a
Greg Lorenzen <gregorylorenzen@gmail.com>
2025-03-21 11:57:22 -0700

    Add get_user_by_email route

--- app/routers/user.py
+++ app/routers/user.py
@@ -2,8 +2,13 @@ from typing import List, Optional
 from fastapi import APIRouter, Depends, Query
 from sqlalchemy.orm import Session
 from app.db import get_db
-from app.data.user import create
-from app.schemas.user import UserCreate, UserCreateResponse, UserLogin, User as UserSchema
+from app.data.user import create, get_by_email
+from app.schemas.user import (
+    UserCreate,
+    UserCreateResponse,
+    UserLogin,
+    User as UserSchema,
+)
 
 router = APIRouter(prefix="/user")
 
@@ -14,6 +19,12 @@ def create_user(user: UserCreate, db: Session = Depends(get_db)):
     return {"message": "Created new user", "user": new_user}
 
 
+@router.get("/email", response_model=UserSchema)
+def get_user_by_email(email: str, db: Session = Depends(get_db)):
+    user = get_by_email(db, email)
+    return UserSchema.model_validate(user, from_attributes=True)
+
+
 @router.post("/login")
 def login_user(user: UserLogin, db: Session = Depends(get_db)):
-
+    pass
